233ccf83a85ef75a63f7a178976bab93e5d29f71
Aemonair <v.aemonair@gmail.com>
2017-07-15 08:53:51 +0800

    Air_CPP/25_Bitset/

---
+++ Air_CPP/25_Bitset/25_00.cpp
@@ -0,0 +1,13 @@
+25 STL位标志
+==
+bitset
+vector<bool>
+
+25.1 bitset类
+--
+std::bitset // #include <bitset>
+bitset <4> fourBits;
+bitset <5> fiveBits ("10101");
+bitset <8> eightBitsCopy(eightbits);
+
+Pro 25.1 
